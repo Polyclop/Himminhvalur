cb353252b27178660cbdb9805986b6efbfa29907
yolobob <to97400@orange.fr>
2020-02-15 20:50:38 +0100

    Ajout Modé Base LVL1 + Skybox + placeholders

---
+++ Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Transform tsf;
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tsf = GetComponent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tsf.position = new Vector3(tsf.position.x + ((Input.GetAxis("Horizontal") * speed)), tsf.position.y, tsf.position.z);
+    }
+}
