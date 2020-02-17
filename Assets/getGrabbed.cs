edb81497ed15d7e8ac0d2cc11e3e5e4f05c1dc4d
yolobob <to97400@orange.fr>
2020-02-17 16:27:25 +0100

    Added basic Grab

---
+++ Assets/getGrabbed.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getGrabbed : MonoBehaviour
+{
+    public bool isGrabbed = false;
+    public bool isInside = false;
+    Collider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInside)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                isGrabbed = !isGrabbed;
+                if (isGrabbed)
+                {
+                    Grab();
+                }
+                else Ungrab();
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other is CapsuleCollider)
+        {
+            isInside = true;
+            col = other;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            if (other is CapsuleCollider)
+            {
+                isInside = false;
+            }
+    }
+
+
+
+    void Grab()
+    {
+        this.transform.parent = col.transform;
+    }
+
+    void Ungrab()
+    {
+        this.transform.parent = null;
+    }
+
+
+}
--- Assets/move.cs
+++ Assets/move.cs
@@ -7,15 +7,21 @@ public class move : MonoBehaviour
     Transform tsf;
     public float speed = 1;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        tsf = GetComponent<Transform>();    
+        tsf = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         tsf.position = new Vector3(tsf.position.x + ((Input.GetAxis("Horizontal") * speed)), tsf.position.y, tsf.position.z);
+
+
+
+
     }
 }
