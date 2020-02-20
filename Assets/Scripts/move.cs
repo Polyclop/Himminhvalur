07b6b40cbe0e46d6a1e3bf0329673bca89dc315e
yolobob <to97400@orange.fr>
2020-02-20 17:12:28 +0100

    test GD 02

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -6,13 +6,16 @@ public class move : MonoBehaviour
 {
     Transform tsf;
     public float speed = 1;
+    public float jumpHeight = 20;
+    public bool canJump = true;
 
-
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         tsf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,8 +23,18 @@ public class move : MonoBehaviour
     {
         tsf.position = new Vector3(tsf.position.x + ((Input.GetAxis("Horizontal") * speed)), tsf.position.y, tsf.position.z);
 
-
+        if(Input.GetButtonDown("Jump") && canJump)
+        {
+            canJump = false;
+            rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+        }
 
 
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+
 }
