00cfe46d09a90d0f3ee1b37f0292078531f47478
yolobob <to97400@orange.fr>
2020-03-27 00:55:23 +0100

    Camera Transition is on, Added a F*cking Whale and some Grass + Shader Graphs Cleaning

---
+++ Assets/RoomTransition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class RoomTransition : MonoBehaviour
+{
+
+    public GameObject[] cameras;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other is CapsuleCollider)
+        {
+            for(int i = 0; i< cameras.Length; i++)
+            {
+                cameras[i].SetActive(!cameras[i].activeSelf);
+            }
+        }
+    }
+}
