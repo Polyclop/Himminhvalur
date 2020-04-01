7778a1e19110569da11c12bb07b1a634ddbb7aa7
john <hadrienbasch@gmail.com>
2020-04-01 18:30:24 +0200

    Anim porte qui s'ouvre + rajout étagère salle 3

---
+++ Assets/Scripts/Doortrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doortrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    public bool isInZone = false;
+    Animator zz_button;
+    public Animator porte_point_pivot;
+     void Start()
+     {
+        zz_button = GetComponent<Animator>();
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        isInZone = true;
+    }
+    void OnTriggerExit(Collider col)
+
+    {
+        isInZone = false;
+    }
+     void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && isInZone)
+
+        {
+            zz_button.SetBool("isPushed", true);
+            porte_point_pivot.SetBool("isRotating", true);
+
+        }
+
+        if (zz_button.GetCurrentAnimatorStateInfo(0).normalizedTime >
+           zz_button.GetCurrentAnimatorStateInfo(0).length && zz_button.GetCurrentAnimatorStateInfo(0).IsName("zz_ Bouton"))
+        {
+            
+            zz_button.SetBool("isPushed", false);
+        }
+    }
+}
+
