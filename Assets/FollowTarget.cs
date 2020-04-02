9ad92441d216f8d4ad164a2b044d6e0b4f1b80bd
yolobob <to97400@orange.fr>
2020-04-02 17:16:53 +0200

    fixed errors

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform playerTransform;
+    Vector3 direction;
+    float angle;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = playerTransform.position - transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    }
+}
--- Assets/Tuto/TutoGameEvent.cs
+++ Assets/Tuto/TutoGameEvent.cs
@@ -1,9 +1,16 @@
-﻿
-/// DANS MON GAME EVENT
-/// je déclare mon événement 
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    public class GameEvents : MonoBehaviour
-    { 
+public class TutoGameEvent : MonoBehaviour
+{
+    bool isInside;
+
+
+    /// DANS MON GAME EVENT
+    /// je déclare mon événement 
+
         // Le nom de mon event 
         // et le type de paramètre que je vais transmettre quand je le déclencherai
         public event Action<bool> onBeingSeen;
@@ -16,36 +23,37 @@
                 onBeingSeen(seen);
             }
         }
-    }
-
-/// DANS MON SCRIPT DECLENCHANT L'EVENT
-/// Quand les conditions sont réunies je déclenche l'événement
-
-    private void OnTriggerEnter(Collider other)
-    {
-        isInside = true;
-
-        // j'avais dit avant que je voulais transmettre une variable alors je la transmet
-        GameEvents.current.BeSeen(isInside);
-    }
 
 
-/// DANS MON SCRIPT QUI ECOUTE
-/// Je souscrit au début a l'événement, et je crée une fonction qui se lancera dans ce cas
+    /// DANS MON SCRIPT DECLENCHANT L'EVENT
+    /// Quand les conditions sont réunies je déclenche l'événement
 
-    
-    // Ici je veux récupérer la valeur que je transmet via l'événement
-    bool seen;
+        private void OnTriggerEnter(Collider other)
+        {
+            isInside = true;
 
-    void Start()
-    {
-        // Souscription
-        GameEvents.current.onBeingSeen += OnGettingCaughtScreenShake;
-    }
+            // j'avais dit avant que je voulais transmettre une variable alors je la transmet
+            GameEvents.current.BeSeen(isInside);
+        }
 
-    // Ma fonction qui se lancera automatiquement 
-    void OnGettingCaughtScreenShake(bool caught)
-    {
-        // je récupère ma valeur
-        seen = caught;
-    }
+
+    /// DANS MON SCRIPT QUI ECOUTE
+    /// Je souscrit au début a l'événement, et je crée une fonction qui se lancera dans ce cas
+
+
+        // Ici je veux récupérer la valeur que je transmet via l'événement
+        bool seen;
+
+        void Start()
+        {
+            // Souscription
+            GameEvents.current.onBeingSeen += OnGettingCaughtScreenShake;
+        }
+
+        // Ma fonction qui se lancera automatiquement 
+        void OnGettingCaughtScreenShake(bool caught)
+        {
+            // je récupère ma valeur
+            seen = caught;
+        }
+}
