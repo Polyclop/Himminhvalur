6fac989c098255f1ecc0838b202231bd320a2936
Polyclop <64970644+Polyclop@users.noreply.github.com>
2020-05-13 14:14:02 +0200

    Replaced jauge by barometre, adapted room2 difficulty

--- Assets/Scripts/caughtJauge.cs
+++ Assets/Scripts/caughtJauge.cs
@@ -13,6 +13,13 @@ public class caughtJauge : MonoBehaviour
 
     Slider slider;
 
+    public Image _aiguille;
+    public RectTransform barometreContainer;
+
+    public float _healthValue = 0;
+    [Range(0, 2)]
+    public float baseValueOffset = 0;
+
     //Death Delegate
     public delegate void OnDeathDelegate();
     public static OnDeathDelegate deathDelegate;
@@ -31,6 +38,8 @@ public class caughtJauge : MonoBehaviour
         slider = GetComponent<Slider>();
         GameEvents.current.onBeingSeen += OnGettingCaughtSlider;
         GameEvents.current.onDying += InitBackSlider;
+        
+
     }
 
     // Update is called once per frame
@@ -40,18 +49,19 @@ public class caughtJauge : MonoBehaviour
         {
             if (seen)
             {
-                if (slider.value < 1)
-                    slider.value += increaseRate * Time.deltaTime;
+                if (_healthValue < 1)
+                    _healthValue += increaseRate * Time.deltaTime;
                 else
                     HandleDeath();
             }
             else
             {
-                if (slider.value > 0)
+                if (_healthValue > 0)
                 {
-                    slider.value -= decreaseRate * Time.deltaTime;
+                    _healthValue -= decreaseRate * Time.deltaTime;
                 }
             }
+            HealthChange(_healthValue);
         }
         
     }
@@ -72,8 +82,17 @@ public class caughtJauge : MonoBehaviour
 
     void InitBackSlider(float currentRoom, float deathDuration)
     {
-        slider.value = 0;
+        _healthValue = 0;
+        HealthChange(_healthValue);
         seen = false;
         isDead = false;
+    }
+
+    void HealthChange(float healthValue)
+    {
+        float amount = ((1 + baseValueOffset + healthValue) * 180.0f / 360);
+        _aiguille.fillAmount = amount;
+        float buttonAngle = amount * 360;
+        barometreContainer.localEulerAngles = new Vector3(0, 0, -buttonAngle);
     }
 }
--- Assets/barometre.cs
+++ Assets/barometre.cs
@@ -10,17 +10,20 @@ public class barometre : MonoBehaviour
     public RectTransform button;
 
     public float _healthValue = 0;
-
+    [Range(0, 2)]
+    public float baseValueOffset = 0;
     // Update is called once per frame
 
 
-     void Update()
+    void Update()
     {
         HealthChange(_healthValue);
     }
+
+
     void HealthChange(float healthValue)
     {
-        float amount = (healthValue / 100.0f) * 180.0f / 360;
+        float amount = ((1+ baseValueOffset + healthValue) * 180.0f / 360);
         _bar.fillAmount = amount;
         float buttonAngle = amount * 360;
         button.localEulerAngles = new Vector3(0, 0, -buttonAngle); 
